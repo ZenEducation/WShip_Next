ca94b7adb7c86548ea3ca7d2c29edb139b44368a
Rishabh Saini <rishabhsaini049@gmail.com>
2023-03-10 12:34:55 +0530

    NextAuth Aws Amplify Cognito login and signup

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
-import '@/styles/globals.css'
+import {Amplify} from 'aws-amplify';
+import awsconfig from '../aws-exports';
+import '@aws-amplify/ui-react/styles.css';
 
+
+Amplify.configure({...awsconfig});
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
